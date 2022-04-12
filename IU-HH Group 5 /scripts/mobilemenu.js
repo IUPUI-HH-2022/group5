c225a5499df95990de3c23d9bffa7b12916381f7
qian-27 <83451817+qian-27@users.noreply.github.com>
2022-04-12 12:17:17 +0300

    Starting File
    On each page, I added necessary meta data, inked stylesheet and JS script and added functional menu. I was trying to design the menu, it looks good now, but not as we planned. Still a lot of work to do, but we can start from here.

---
+++ IU-HH Group 5 /scripts/mobilemenu.js
@@ -0,0 +1,12 @@
+// Responsive Navigation Bar
+// https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
